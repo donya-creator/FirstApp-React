2ad03799e44660ae700a2f3cdd73378f237d3d5c
donya <donya21creator@gmail.com>
2025-07-31 11:37:03 +0200

    use HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "./Components/Layout/Layout";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import About from "./Components/About/About";
 import Home from "./Components/Home/Home";
 import Contact from "./Components/Contact/Contact";
 import Portfolio from "./Components/Portfolio/Portfolio";
 
 function App() {
-  const routers = createBrowserRouter([
-    {
-      path: "",
-      element: <Layout />,
-      children: [
-        { index: true, element: <Home /> },
-        { path: "about", element: <About /> },
-        { path: "contact", element: <Contact /> },
-        { path: "portfolio", element: <Portfolio /> },
-      ],
-    },
-  ]);
   return (
-    <>
-      <RouterProvider router={routers} />
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="portfolio" element={<Portfolio />} />
+      </Route>
+    </Routes>
   );
 }
 
